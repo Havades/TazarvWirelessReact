e5b3ef41e248f1845f8a6fc79181254a39af4927
Omid Dadvar <omiddadvar94@gmail.com>
2021-11-21 17:24:05 +0330

    drawer added :wink:

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,22 +1,47 @@
 import React from 'react'
 import { Login , Main , Message , Setting , Profile } from './../screens';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+// import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
 const appName = "نرم افزار بی سیم تذرو"
-
-const Stack = createNativeStackNavigator();
+const routeNames = {
+    Main : "نرم افزار بی سیم تذرو",
+    Profile: "کاربری",
+    Login: "ورود",
+    Message: "پیام ها",
+    Setting: "تنظیمات",
+}
+// const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
 
 const Navigation = () => {
     return (
         <NavigationContainer>
-            <Stack.Navigator initialRouteName="Setting">
-                <Stack.Screen name="Main" component={Main} options={{ title: appName }}/>
-                <Stack.Screen name="Profile" component={Profile} />
-                <Stack.Screen name="Login" component={Login} />
-                <Stack.Screen name="Message" component={Message} />
-                <Stack.Screen name="Setting" component={Setting} />
-            </Stack.Navigator>
+            <Drawer.Navigator initialRouteName="Setting"
+                screenOptions={{
+                    drawerPosition : 'right',
+                    drawerStyle: {
+                        backgroundColor: '#36454F',
+                        alignItems : 'stretch',
+                      },
+                      headerStyle : {
+                          height : 60,
+                          backgroundColor : '#778899',
+                          color : 'white'
+                      }
+                     , drawerLabelStyle : {
+                        fontSize : 20,
+                        color : 'white'
+                    }
+                }}
+                >
+                <Drawer.Screen name="Main" component={Main} options={{ title: routeNames.Main}}/>
+                <Drawer.Screen name="Profile" component={Profile} options={{ title: routeNames.Profile }}/>
+                <Drawer.Screen name="Login" component={Login} options={{ title: routeNames.Login }}/>
+                <Drawer.Screen name="Message" component={Message} options={{ title: routeNames.Message }}/>
+                <Drawer.Screen name="Setting" component={Setting} options={{ title: routeNames.Setting }}/>
+            </Drawer.Navigator>
       </NavigationContainer>
     )
 }
