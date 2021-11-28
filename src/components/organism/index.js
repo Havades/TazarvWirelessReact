2474f57492eee8ffa4e2d79ed0affee29ae393c5
Omid Dadvar <omiddadvar94@gmail.com>
2021-11-28 17:05:06 +0330

    some dummy added , little change in UI :weary:

--- src/components/organism/index.js
+++ src/components/organism/index.js
@@ -1,8 +1,11 @@
 import LoginOrganism from "./login_organism";
 import Waiting from './waiting';
 import Room from './room';
+import VoiceMessage from './voice_message';
+
 export {
     LoginOrganism,
     Waiting,
-    Room
+    Room,
+    VoiceMessage
 }
---
+++ src/components/organism/voice_message/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+import styles from './style'
+const VoiceMessage = () => {
+    return (
+        <View style={styles.container}>
+            <Text></Text>
+        </View>
+    )
+}
+
+export default VoiceMessage
